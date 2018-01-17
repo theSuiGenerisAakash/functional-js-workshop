1e7fbe1ebbec66105fa8abac70d9f9fe8a3d4ea8
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-17 11:27:27 +0530

    Added tests for reduce

---
+++ reduceProg.js
@@ -0,0 +1,7 @@
+function reduceFunc(inputWords){
+  return inputWords.reduce((stringAccumulator, currentString) => { stringAccumulator[currentString] =  stringAccumulator[currentString]+1 || 1; return stringAccumulator; } , {});
+}
+module.exports = reduceFunc;
+
+// initialValue[Optional]
+// Value to use as the first argument to the first call of the callback. If no initial value is supplied, the first element in the array will be used. Calling reduce() on an empty array without an initial value is an error.
